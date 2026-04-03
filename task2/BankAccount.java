59beece65291446b3e851dcdc033b84abdc75028
Brain17 <b.daminov@newuu.uz>
2026-04-03 22:37:01 +0500

    Implement BankAccount class with withdrawal functionality

---
+++ task2/BankAccount.java
@@ -0,0 +1,16 @@
+public class BankAccount {
+    private double balance;
+    public BankAccount(double initialBalance) {
+        balance=initialBalance;
+    }
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if (amount>balance) {
+            throw new InsufficientFundsException(amount-balance);
+        }
+        balance-=amount;
+        System.out.printf("Withdrew $%.2f.Remaining balance; $%.2f\n", amount, balance);
+    }
+    public double getBalance() {
+        return balance;
+    }
+}
